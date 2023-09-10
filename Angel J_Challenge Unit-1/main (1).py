c45a24922de6c7d0d4d733ffe49b68869783aa4f
astvu21021022u18004 <144608429+astvu21021022u18004@users.noreply.github.com>
2023-09-10 20:55:33 +0530

    Add files via upload

---
+++ Angel J_Challenge Unit-1/main (1).py
@@ -0,0 +1,23 @@
+
+
+
+# Leap Year Program
+
+def isLeapYear(year):
+  if(year % 4==0 and year % 100!=0) or year % 400:
+   return True
+  else:
+   return False
+year=int(input("Enter a year:"))
+          
+if isLeapYear(year):
+   print ('{} is a leap year.'.format(year))
+else:    
+  print ('{} is not a leap year.'.format(year))
+    
+           
+  
+
+     
+    
+    
